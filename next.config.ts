bb09613e63b54cf8ba6cf341f8a2b815e82a7aac
sungho1103 <galtensh@gmail.com>
2026-05-11 18:52:57 +0900

    Configure serverExternalPackages and webpack settings

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["firebasestorage.googleapis.com"],
+  serverExternalPackages: ["pdf-parse"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // canvas is an optional dependency of pdf-parse, ignore it
+      config.externals = [...(config.externals || []), "canvas"];
+    }
+    return config;
   },
 };
 
