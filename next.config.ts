f1d72535b7083e83ae491fa457db083250bf293e
sungho1103 <galtensh@gmail.com>
2026-05-11 19:18:56 +0900

    Simplify next.config.ts by removing configurations
    Removed serverExternalPackages and webpack configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  serverExternalPackages: ["pdf-parse"],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // canvas is an optional dependency of pdf-parse, ignore it
-      config.externals = [...(config.externals || []), "canvas"];
-    }
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
